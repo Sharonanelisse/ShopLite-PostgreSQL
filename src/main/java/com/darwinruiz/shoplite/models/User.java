d0dc85100fddcc2981ea338d2fb3d2a0cfa0651b
Sharon <168245948+Sharonanelisse@users.noreply.github.com>
2025-08-20 19:26:07 -0600

    update user

--- src/main/java/com/darwinruiz/shoplite/models/User.java
+++ src/main/java/com/darwinruiz/shoplite/models/User.java
@@ -1,25 +1,62 @@
 package com.darwinruiz.shoplite.models;
 
-public class User {
-    private final String email;
-    private final String password;
-    private final String role; // "USER" | "ADMIN"
 
-    public User(String email, String password, String role) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String role;   // "ADMIN" o "USER"
+    private boolean active;
+
+    public User() {
     }
 
-    public String getEmail() {
-        return email;
+    public User(Integer id, String username, String password, String role, boolean active) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
+
+    // getters/setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getRole() {
         return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
